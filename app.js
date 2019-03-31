c54d98cc6b3322e471b69c31d2f021cc395796cb
Ajoy Oommen <ajoy.oommen@gmail.com>
2019-03-31 23:09:06 +0530

    Add pre-processor to clean dataset

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ function loadTimeline(dataset, groups, element) {
 
     // Configuration for the Timeline
     var options = {
-        width: "90%",
-        zoomKey: 'ctrlKey'
+        zoomKey: 'ctrlKey',
+        zoomMax: 15778800000000,
+        zoomMin: 31557600000
     };
 
     // Create a Timeline
@@ -18,11 +19,40 @@ var container = document.getElementById('visualization');
 var indicator = document.getElementById('span-status-indicator');
 indicator.textContent = "Loading data ...";
 
+
+function process_data(dataset) {
+    dataset.data.forEach(function (element) {
+        //console.log(element)
+        if (element.end.trim() === "") {
+            element.end = null;
+        }
+    });
+
+    dataset.groups.forEach(function (element) {
+        if (element.visible.toLowerCase() === "false") {
+            element.visible = false;
+        } else {
+            element.visible = true;
+        }
+
+        if (element.nestedGroups.trim() !== "") {
+            element.nestedGroups = element.nestedGroups.split(',');
+            element.showNested = false;
+        }
+    });
+
+    return {
+        data: dataset.data,
+        groups: dataset.groups
+    };
+}
+
 fetch('https://esm7sau4p5.execute-api.us-east-1.amazonaws.com/default/timeline').then(function (response) {
     return response.json();
 }).then(function (dataset) {
-    console.log(dataset.data)
 
-    loadTimeline(dataset.data, dataset.groups, container);
+    results = process_data(dataset);
+
+    loadTimeline(results.data, results.groups, container);
     indicator.textContent = "";
 })
